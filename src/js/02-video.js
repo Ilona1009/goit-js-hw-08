fbaa0dc91d0237d40073268c37bfd90c5efebf1d
ilona1009 <maliarenko.ilona@gmail.com>
2022-10-30 09:05:34 +0100

    2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
 
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+    const iframe = document.querySelector('#vimeo-player');
+    const player = new Vimeo.Player(iframe);
+
+    const KEY_STORAGE = 'videoplayer-current-time';
+
+    player.on('timeupdate', throttle(event => {
+    localStorage.setItem(KEY_STORAGE, JSON.stringify(event.seconds));
+}, 1000)
+);
+
+    // player.getVideoTitle().then(function(title) {
+    //     console.log('title:', title);
+    // });
